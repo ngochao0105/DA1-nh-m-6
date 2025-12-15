80399978a5489043823628197595cd6727e3cf34
ngochao0105 <ngochao21012005@gmail.com>
2025-12-15 13:08:11 +0700

    lastcommit

--- commons/env.php
+++ commons/env.php
@@ -9,6 +9,6 @@ define('DB_HOST'    , 'localhost');
 define('DB_PORT'    , 3306);
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_NAME'    , 'hao1');  // Tên database
+define('DB_NAME'    , 'hao2');  // Tên database
 
 define('PATH_ROOT'    , __DIR__ . '/../');
